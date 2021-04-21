24e49bf92e7887549d6949e818c0a6f7ba474cf5
Nachat-dev <71816429+Nachat-dev@users.noreply.github.com>
2021-04-21 16:37:54 +0700

    img

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -43,7 +43,7 @@
             <center>
 
                 <input type="hidden" name="_token" value="{{ csrf_token() }}" />
-                <img src="/logokumon.svg" alt="Image" />
+                <img src="/assets/logokumon.png" alt="Image" />
                 <hr class="login-hr" />
                 <h4 class="form-signin-heading">Advanced Student Forum</h4>
                 <br />
